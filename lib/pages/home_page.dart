15615e98e12e0306cc8db58d1ce77eb0932d1dfe
MintMP <hamin908@gmail.com>
2024-09-26 17:29:21 +0900

    [#19] debug: 브랜치 충돌 해결 home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -155,7 +155,7 @@ class _HomePageState extends State<HomePage> {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                            builder: (context) => const ProfileSettingPage(fromHomePage: true),
+                          builder: (context) => const ProfileSettingPage(fromHomePage: true),
                         ),
                       );
                     },
