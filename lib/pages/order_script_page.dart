45aabe09542dfe768fa51bc7e85784663911c592
YSMin <zxczxv7624@gmail.com>
2024-09-30 17:50:47 +0900

    [#26] fix: order_script_page
    - 글씨 크기 변경

--- lib/pages/order_script_page.dart
+++ lib/pages/order_script_page.dart
@@ -12,7 +12,7 @@ class OrderScriptPageState extends State<OrderScriptPage> {
   @override
   Widget build(BuildContext context) {
     const double appBarHeight = 60.0;
-    final double screenHeight = MediaQuery.of(context).size.height; // 전체 화면 높이
+    final double screenHeight = MediaQuery.of(context).size.height;
     final double bodyHeight = screenHeight - appBarHeight;
 
     return Scaffold(
@@ -44,7 +44,7 @@ class OrderScriptPageState extends State<OrderScriptPage> {
         ),
       ),
       body: Container(
-        height: bodyHeight, // Body의 높이를 AppBar를 제외한 높이로 설정
+        height: bodyHeight,
         child: Column(
           children: [
             Expanded(
@@ -56,19 +56,19 @@ class OrderScriptPageState extends State<OrderScriptPage> {
                   crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
                     Transform.rotate(
-                      angle: 3.14159, // 텍스트 뒤집기
+                      angle: 3.14159,
                       child: const Text(
                         "hello, I want to order",
-                        style: TextStyle(fontSize: 17),
+                        style: TextStyle(fontSize: 24),
                       ),
                     ),
                     const SizedBox(height: 10),
                     Transform.rotate(
-                      angle: 3.14159, // 텍스트 뒤집기
+                      angle: 3.14159,
                       child: const Text(
                         'Order',
                         style: TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
+                            fontSize: 30, fontWeight: FontWeight.bold),
                       ),
                     ),
                     const SizedBox(
@@ -78,10 +78,9 @@ class OrderScriptPageState extends State<OrderScriptPage> {
                 ),
               ),
             ),
-            // 가운데 선 화면 중앙 정렬
             Center(
               child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 20), // 좌우 여백 설정
+                margin: const EdgeInsets.symmetric(horizontal: 20),
                 child: const Divider(
                   thickness: 2,
                   color: Colors.grey,
@@ -89,33 +88,34 @@ class OrderScriptPageState extends State<OrderScriptPage> {
                 ),
               ),
             ),
-            // 하단 텍스트 왼쪽 정렬
             Expanded(
               child: Container(
-                alignment: Alignment.topLeft, // 텍스트를 왼쪽 정렬
-                padding: const EdgeInsets.only(left: 20), // 왼쪽 여백 추가
-                child: Column(
+                alignment: Alignment.topLeft,
+                padding: const EdgeInsets.only(left: 20),
+                child: const Column(
                   mainAxisSize: MainAxisSize.min,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    const SizedBox(
+                    SizedBox(
                       height: 10,
                     ),
-                    const Text(
+                    Text(
                       '당신의 주문',
                       style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                     ),
-                    const SizedBox(height: 10),
-                    const Text(
+                    SizedBox(height: 10),
+                    Text(
                       "주문하고 싶습니다.",
-                      style: TextStyle(fontSize: 17),
+                      style: TextStyle(fontSize: 24),
                     ),
                   ],
                 ),
               ),
             ),
-          const SizedBox(height: 70,)
+            const SizedBox(
+              height: 70,
+            )
           ],
         ),
       ),
@@ -144,7 +144,7 @@ class OrderScriptPageState extends State<OrderScriptPage> {
               child: Text(
                 "Bon appétit!",
                 style: TextStyle(
-                  color: Colors.white, // 텍스트 색상 설정
+                  color: Colors.white,
                 ),
               ),
             ),
