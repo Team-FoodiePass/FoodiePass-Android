c03d653feb706d3fbfb313a52e0d00bdda5ef007
YSMin <zxczxv7624@gmail.com>
2024-09-30 17:48:37 +0900

    [#26] fix: order_script_page
    - 가운데 정렬 문제 일부 해결

--- lib/pages/order_script_page.dart
+++ lib/pages/order_script_page.dart
@@ -8,111 +8,116 @@ class OrderScriptPage extends StatefulWidget {
 }
 
 class OrderScriptPageState extends State<OrderScriptPage> {
+
   @override
   Widget build(BuildContext context) {
+    const double appBarHeight = 60.0;
+    final double screenHeight = MediaQuery.of(context).size.height; // 전체 화면 높이
+    final double bodyHeight = screenHeight - appBarHeight;
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
-        toolbarHeight: 60.0,
+        toolbarHeight: appBarHeight,
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             IconButton(
-                onPressed: () {
-                  clickBackButton();
-                },
-                icon: const Icon(
-                  Icons.arrow_back_ios,
-                  size: 30,
-                )),
+              onPressed: () {
+                clickBackButton();
+              },
+              icon: const Icon(
+                Icons.arrow_back_ios,
+                size: 30,
+              ),
+            ),
             IconButton(
-                onPressed: () {
-                  clickHomeButton();
-                },
-                icon: const Icon(
-                  Icons.home,
-                  size: 30,
-                ))
+              onPressed: () {
+                clickHomeButton();
+              },
+              icon: const Icon(
+                Icons.home,
+                size: 30,
+              ),
+            ),
           ],
         ),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              height: MediaQuery.of(context).size.height,
-              child: SingleChildScrollView(
-                child: Transform(
-                  alignment: Alignment.center,
-                  transform: Matrix4.rotationZ(3.141592),
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 20,
-                      ),
-                      const Text(
-                        'hello, I want to order',
-                        style: TextStyle(fontSize: 17),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Column(
-            children: [
-              Transform(
-                alignment: Alignment.center,
-                transform: Matrix4.rotationZ(3.141592),
-                child: const Text(
-                  'Order',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Center(
-                child: Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  child: const Divider(
-                    thickness: 2,
-                    color: Colors.grey,
-                  ),
-                ),
-              ),
-              const Text(
-                '당신의 주문',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-              )
-            ],
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Expanded(
-            child: Container(
-              alignment: Alignment.topCenter,
-              height: MediaQuery.of(context).size.height,
-              child: SingleChildScrollView(
+      body: Container(
+        height: bodyHeight, // Body의 높이를 AppBar를 제외한 높이로 설정
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                alignment: Alignment.bottomRight,
+                padding: const EdgeInsets.only(right: 20),
                 child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.end,
                   children: [
-                    Container(
-                      height: 20,
+                    Transform.rotate(
+                      angle: 3.14159, // 텍스트 뒤집기
+                      child: const Text(
+                        "hello, I want to order",
+                        style: TextStyle(fontSize: 17),
+                      ),
                     ),
-                    const Text(
-                      'hello, I want to order',
-                      style: TextStyle(fontSize: 17),
+                    const SizedBox(height: 10),
+                    Transform.rotate(
+                      angle: 3.14159, // 텍스트 뒤집기
+                      child: const Text(
+                        'Order',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
                     )
                   ],
                 ),
               ),
             ),
-          ),
-        ],
+            // 가운데 선 화면 중앙 정렬
+            Center(
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 20), // 좌우 여백 설정
+                child: const Divider(
+                  thickness: 2,
+                  color: Colors.grey,
+                  height: 0,
+                ),
+              ),
+            ),
+            // 하단 텍스트 왼쪽 정렬
+            Expanded(
+              child: Container(
+                alignment: Alignment.topLeft, // 텍스트를 왼쪽 정렬
+                padding: const EdgeInsets.only(left: 20), // 왼쪽 여백 추가
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      '당신의 주문',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      "주문하고 싶습니다.",
+                      style: TextStyle(fontSize: 17),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          const SizedBox(height: 70,)
+          ],
+        ),
       ),
       floatingActionButton: Container(
         width: double.infinity,
